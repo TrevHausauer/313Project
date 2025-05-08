87d212d004d5dce16003daf00a9990970d0a0d9e
DirtyDanPutnam <daniel.putnam@ndus.edu>
2025-05-08 00:34:56 -0500

    fixed calendar fr

--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -2,30 +2,54 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CalendarEventInterface } from '../calendar-event-interface';
 import { CalendarService } from '../calendar.service';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-add-event',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './add-event.component.html',
   styleUrl: './add-event.component.css',
 })
 export class AddEventComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
-  date!: string;
+  sDate: string = '';
+  sTime: string = '';
+  eDate: string = '';
+  eTime: string = '';
+  priority: string = '';
+  title: string = '';
+  description: string = '';
 
   calendarService = inject(CalendarService);
-  event: CalendarEventInterface = { id: '', title: '', date: '', endDate: '' };
+  authService = inject(AuthService);
 
-  addEvent(sDate: string, eDate: string) {
-    this.event.date = sDate;
-    this.event.endDate = eDate;
+  event: CalendarEventInterface = {
+    id: '',
+    title: '',
+    date: '',
+    endDate: '',
+    priority: '',
+    description: '',
+    userID: '',
+  };
+
+  addEvent() {
+    const startDateTimeString = `${this.sDate}T${this.sTime}`;
+    const endDateTimeString = `${this.eDate}T${this.eTime}`;
+    this.event.date = startDateTimeString;
+    this.event.endDate = endDateTimeString;
+    this.event.title = this.title;
+    this.event.priority = this.priority;
+    this.event.description = this.description;
+    this.event.userID = this.authService.getUser()?.uid;
     this.calendarService.addEvent(this.event);
     this.resetForm();
   }
 
   ngOnInit(): void {
-    this.date = this.route.snapshot.paramMap.get('date')!;
+    this.sDate = this.route.snapshot.paramMap.get('date')!;
   }
 
   resetForm() {
@@ -34,6 +58,9 @@ export class AddEventComponent implements OnInit {
       title: '',
       date: '',
       endDate: '',
+      priority: '',
+      description: '',
+      userID: '',
     };
   }
 }
--- src/app/calendar-page/calendar-page.component.ts
+++ src/app/calendar-page/calendar-page.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CalendarEventInterface } from '../calendar-event-interface';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { NgxCalendarWidgetModule } from '@localia/ngx-calendar-widget';
 import { CalendarModule } from '../calendar/calendar.module';
+import { CalendarService } from '../calendar.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-calendar-page',
@@ -10,23 +12,13 @@ import { CalendarModule } from '../calendar/calendar.module';
   templateUrl: './calendar-page.component.html',
   styleUrl: './calendar-page.component.css',
 })
-export class CalendarPageComponent {
+export class CalendarPageComponent implements OnInit {
+  calendarService = inject(CalendarService);
+  authService = inject(AuthService);
+
   constructor(private router: Router) {}
 
-  events: CalendarEventInterface[] = [
-    {
-      id: 1,
-      title: 'Team Meeting',
-      date: '2025-05-01T10:00:00',
-      endDate: '2025-05-01T12:00:00',
-    },
-    {
-      id: 2,
-      title: 'Conference',
-      date: '2025-05-05T09:00:00',
-      endDate: '2025-05-07T17:00:00',
-    },
-  ];
+  events: CalendarEventInterface[] = [];
 
   onAddEvent(date: string) {
     const dateString = date.toString().split('T')[0];
@@ -35,5 +27,13 @@ export class CalendarPageComponent {
   onEventSelect(event: CalendarEventInterface) {
     console.log('Event clicked:', event);
     // Implement your event handling logic here
+  }
+
+  ngOnInit(): void {
+    this.calendarService.getEvents().subscribe((data) => {
+      this.events = data.filter(
+        (event) => event.userID === this.authService.getUser()?.uid
+      );
+    });
   }
 }
